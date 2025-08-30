739c8618fe9721d2ba4d8544eb4167775f539784
Neurobots1 <neurobots@classomption.qc.ca>
2025-08-30 15:52:47 -0400

    Noscentia 1.1.11 SiteMap 1

--- astro.config.mjs
+++ astro.config.mjs
@@ -9,7 +9,7 @@ import mdx from "@astrojs/mdx";
 // https://astro.build/config
 export default defineConfig({
   // https://docs.astro.build/en/guides/images/#authorizing-remote-images
-  site: "https://Noscentia.com",
+  site: "https://noscentia.com",
   image: {
     domains: ["images.unsplash.com"],
   },
